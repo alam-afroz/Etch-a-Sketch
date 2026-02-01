5c606b8715160c2c71aac13fcc5a7164380f516f
Afroz Alam <236445412+afrozalam0699-lab@users.noreply.github.com>
2026-02-01 17:15:41 +0530

    added resize option for the sketchpad

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const container = document.querySelector(".container");
 const gridSize = document.querySelector(".gridSize");
 
 //function to create the grid
-grid(16, 16);
-function grid(a, b) {
-  for (let j = 0; j < a; j++) {
-    for (let i = 0; i < b; i++) {
+let d = 0;
+
+let size;
+grid(16);
+function grid(size) {
+  for (let j = 0; j < size; j++) {
+    for (let i = 0; i < size; i++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.textContent = i;
-      box.style.width = 512 / a + "px";
-      box.style.height = 512 / a + "px";
+
+      box.style.width = 512 / size + "px";
+      box.style.height = 512 / size + "px";
       container.appendChild(box);
       box.addEventListener("mouseenter", () => {
         box.style.backgroundColor = "black";
@@ -20,10 +23,16 @@ function grid(a, b) {
   }
 }
 //creating new grid
-let size;
-function sizeOfGrid() {
-  let size = prompt("enter size of grid");
-  grid(size, size);
-}
+
+function sizeOfGrid() {}
+
 //getting the user value for grid size
-gridSize.addEventListener("click", sizeOfGrid);
+gridSize.addEventListener("click", () => {
+  size = Number(prompt("enter size of grid"));
+  if (size < 100 && size > 0) {
+    container.innerHTML = "";
+    grid(size);
+  } else {
+    prompt("Enter a valid number within the range (1-100)");
+  }
+});
